50794bcca2bacebfe736e26d6074fb1bd19a90b2
binos61201989 <109301999+binos61201989@users.noreply.github.com>
2024-09-19 12:31:08 +0000

    Changes in html and app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_worl():
-  return "Hello Python webapp"
+  return render_template('home.html')
 
 print(__name__)
 
