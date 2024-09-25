1a5eaa77f2e5f017c2e120727f9f356c95dcb8ca
binos61201989 <109301999+binos61201989@users.noreply.github.com>
2024-09-25 12:48:48 +0000

    Additional devlpment

--- app.py
+++ app.py
@@ -2,9 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS=[
+{
+  'id':1,
+  'title':'Data Analyst',
+  'location':'Bengaluru,India',
+  'salary':'Rs . 15,00,000'
+},
+
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi,India',
+    'salary':'Rs . 15,00,000'
+  },
+
+  {
+    'id':3,
+    'title':'Backend Engineer',
+    'location':'Remote',
+    'salary':'Rs . 12,00,000'
+  },
+
+  {
+    'id':4,
+    'title':'Frontend Engineer',
+    'location':'Sanfransico,USA',
+    #'salary':'$120,00,000'
+  }
+ 
+]
+
 @app.route("/")
 def hello_worl():
-  return render_template('index.html')
+  return render_template('index.html',
+                        jobs=JOBS,
+                        company_name='Jovian')
 
 print(__name__)
 
